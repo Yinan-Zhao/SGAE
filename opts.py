c6af770a4fe81450f972363d1594c816efba1b21
Zhao <alexzhao@cs.utexas.edu>
2019-10-23 14:20:56 -0500

    first commit

--- opts.py
+++ opts.py
@@ -37,7 +37,7 @@ def parse_opt():
                                               Note: this file contains absolute paths, be careful when moving files around;
                         'model.ckpt-*'      : file(s) with model definition (created by tf)
                     """)
-    parser.add_argument('--cached_tokens', type=str, default='coco-train-idxs',
+    parser.add_argument('--cached_tokens', type=str, default='vizwiz-train-idxs',
                     help='Cached token file for calculating cider score during self critical training.')
     parser.add_argument('--train_split', type=str, default='train',
                         help='which split used to train')
