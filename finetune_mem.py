74ea1be6ded52091321b9ef4d4678405b89114ef
Zhao <alexzhao@cs.utexas.edu>
2019-10-23 23:36:28 -0500

    first commit

--- finetune_mem.py
+++ finetune_mem.py
@@ -36,8 +36,8 @@ def load_para(model, ckpt_file, load_to_cpu=True):
             p.data.copy_(ip.data) # Copy the data of parameters
         else:
             print('{} -shape {} ,{}'.format(n, (p.shape), (ip.shape))) 
-            #p.data[:10369].copy_(ip.data[:10369])
-            #p.data[-1].copy_(ip.data[-1])
+            p.data[:10369].copy_(ip.data[:10369])
+            p.data[12612].copy_(ip.data[10369])
     return model
 
 def add_summary_value(writer, key, value, iteration):
